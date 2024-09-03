75409f471e3196cacd41fcc43d31c6e4eb78b357
valive421 <74894343+valive421@users.noreply.github.com>
2024-09-03 13:51:57 +0530

    Update settings.py

--- skill_dashboard/settings.py
+++ skill_dashboard/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'trrLGX13im/hKL4NWngchGc9zhYIAzNr8NtQnzYg'  # Replace with a real s
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','webapp-1-3cpn.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
