8784e5cde2bbdec2e05ea43bb21470ff3dce9130
valive421 <74894343+valive421@users.noreply.github.com>
2024-09-03 17:35:07 +0530

    Update settings.py

--- skill_dashboard/settings.py
+++ skill_dashboard/settings.py
@@ -52,11 +52,11 @@ WSGI_APPLICATION = 'skill_dashboard.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQL_DATABASE', 'auth_user'),
-        'USER': os.getenv('MYSQL_USER', 'avnadmin'),
-        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'AVNS_z7rcVxzd0o4Hkjxr5jd'),
-        'HOST': os.getenv('MYSQL_HOST', 'mysql-3a6c982d-valive421-ee77.k.aivencloud.com'),
-        'PORT': os.getenv('MYSQL_PORT', '28129'),
+        'NAME': os.getenv('MYSQL_DATABASE', 'sql12729233'),
+        'USER': os.getenv('MYSQL_USER', 'sql12729233'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'aJ2ir8vxJS'),
+        'HOST': os.getenv('MYSQL_HOST', 'http://sql12.freesqldatabase.com'),
+        'PORT': os.getenv('MYSQL_PORT', '3306'),
     }
 }
 
