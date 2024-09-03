5c42ae741f5e5f095405dab9d454bc2707198e26
valive421 <74894343+valive421@users.noreply.github.com>
2024-09-03 15:01:13 +0530

    Update settings.py

--- skill_dashboard/settings.py
+++ skill_dashboard/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'trrLGX13im/hKL4NWngchGc9zhYIAzNr8NtQnzYg'  # Replace with a real secret key
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
